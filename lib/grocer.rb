0dfe44957dfb4b30a8c93a463f2b82db30fd8f6c
Anna Nicole Reed <reedanna@mail.com>
2020-07-02 15:35:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
   new_item_index = 0
   
   cart.each do |item|
-    if find_item_in_name_in_collection(item[:name], new_cart)
+    if find_item_by_name_in_collection(item[:name], new_cart)
       #if it is already in the new cart
       new_item_index = new_cart.find {|new_item| new_item == item[:name]}
       new_cart[new_item_index][:count] += 1
